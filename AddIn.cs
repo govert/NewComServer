1806ecb05bad525704286cdd2ed5cc7e0d4ffeee
Govert van Drimmelen <govert@dnakode.com>
2023-05-10 22:15:46 +0200

    Add project files.

---
+++ AddIn.cs
@@ -0,0 +1,193 @@
+﻿using System.Reflection;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using ExcelDna.ComInterop;
+using ExcelDna.Integration;
+using System.Collections;
+using Microsoft.Win32;
+
+namespace NewComServer
+{
+    [ComVisible(false)]
+    public class ExcelAddin : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            // ComServer.DllRegisterServer();
+        }
+        public void AutoClose()
+        {
+            // ComServer.DllUnregisterServer();
+        }
+    }
+
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class ComLibrary
+    {
+        public string ComLibraryHello()
+        {
+            return "Hello from DnaComServer.ComLibrary";
+        }
+
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+    }
+
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class ComLibrary2
+    {
+        public string ComLibrary2Hello()
+        {
+            return "Hello from DnaComServer.ComLibrary2";
+        }
+
+        public double Add2(double x, double y)
+        {
+            return x + y;
+        }
+    }
+
+
+
+    public static class Functions
+    {
+        [ExcelFunction]
+        public static object DnaComServerHello()
+        {
+            return "Hello from DnaComServer!";
+        }
+
+        [ExcelFunction("Dumps the list of COM server classes that were found at initialization")]
+        public static object DnaDumpComServerTypes()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // Get the type of the ComServer class.
+            Type comServerType = Type.GetType("ExcelDna.ComInterop.ComServer, ExcelDna.Integration");
+
+            // Get the field info of the registeredComClassTypes field.
+            FieldInfo registeredComClassTypesField = comServerType.GetField("registeredComClassTypes", BindingFlags.NonPublic | BindingFlags.Static);
+
+            // Get the value of the field. This will be the list of ExcelComClassType objects.
+            var registeredComClassTypes = (IList)registeredComClassTypesField.GetValue(null);
+
+            // Iterate through the list.
+            foreach (var item in registeredComClassTypes)
+            {
+                Type itemType = item.GetType();
+                FieldInfo[] fields = itemType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+                foreach (FieldInfo field in fields)
+                {
+                    string name = field.Name;
+                    object value = field.GetValue(item);
+                    sb.AppendLine($"{name}: {value}");
+                }
+
+                sb.AppendLine("-----------");
+            }
+
+            var result = sb.ToString();
+            return result;
+        }
+        
+            // Look at both the user and machine hive, in case the COM server was registered for the current user.
+            // Find the ProgId, from there the ClsId, and then the registry entries for the ClsId.
+            // Show the complete subtrees from ProgId and ClsId, and show where they are not present.
+            // Look for both 32-bit and 64-bit entries
+            // Dump the result as a nicely formatted string showing the hierarchy
+        [ExcelFunction("Dumps the registry entries for a COM server class")]
+        public static object DnaDumpComServerRegistry(string progId)
+        {
+            if (string.IsNullOrEmpty(progId))
+                return ExcelError.ExcelErrorValue;
+
+            StringBuilder result = new StringBuilder();
+            RegistryHive[] rootKeys = { RegistryHive.ClassesRoot, RegistryHive.CurrentUser, RegistryHive.LocalMachine };
+            RegistryView[] views = { RegistryView.Registry32, RegistryView.Registry64 };
+
+            foreach (var root in rootKeys)
+            {
+                foreach (var view in views)
+                {
+                    using (var baseKey = RegistryKey.OpenBaseKey(root, view))
+                    {
+                        using (var progIdKey = baseKey.OpenSubKey(progId))
+                        {
+                            if (progIdKey != null)
+                            {
+                                result.AppendLine($"Found ProgId {progId} at {root}\\{progId}, {view}");
+                                DumpRegistryKey(progIdKey, result, "  ");
+
+                                using (var clsIdSubKey = progIdKey.OpenSubKey("CLSID"))
+                                {
+                                    string clsId = clsIdSubKey?.GetValue("") as string;
+                                    if (clsId != null)
+                                    {
+                                        result.AppendLine($"Found ClsId {clsId} for ProgId {progId}, {view}");
+
+                                        // Check ClsId in all root keys
+                                        foreach (var clsRoot in rootKeys)
+                                        {
+                                            using (var clsBaseKey = RegistryKey.OpenBaseKey(clsRoot, view))
+                                            {
+                                                string clsKeyPath = clsRoot == RegistryHive.ClassesRoot ? $"CLSID\\{clsId}" : $"Software\\Classes\\CLSID\\{clsId}";
+                                                using (var clsIdKey = clsBaseKey.OpenSubKey(clsKeyPath))
+                                                {
+                                                    if (clsIdKey != null)
+                                                    {
+                                                        result.AppendLine($"Found ClsId {clsId} at {clsRoot}\\{clsKeyPath}, {view}");
+                                                        DumpRegistryKey(clsIdKey, result, "  ");
+                                                    }
+                                                    else
+                                                    {
+                                                        result.AppendLine($"ClsId {clsId} not found at {clsRoot}\\{clsKeyPath}, {view}");
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        result.AppendLine($"ClsId not found for ProgId {progId}, {view}");
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                result.AppendLine($"ProgId {progId} not found at {root}\\{progId}, {view}");
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private static void DumpRegistryKey(RegistryKey key, StringBuilder result, string indent)
+        {
+            foreach (string valueName in key.GetValueNames())
+            {
+                object value = key.GetValue(valueName);
+                string valueAsString = value as string ?? "(non-string value)";
+                result.AppendLine($"{indent}{valueName} = {valueAsString}");
+            }
+
+            foreach (string subKeyName in key.GetSubKeyNames())
+            {
+                using (RegistryKey subKey = key.OpenSubKey(subKeyName))
+                {
+                    result.AppendLine($"{indent}{subKeyName}:");
+                    DumpRegistryKey(subKey, result, indent + "  ");
+                }
+            }
+        }
+
+    }
+}
---
+++ Ribbon.cs
@@ -0,0 +1,42 @@
+﻿using ExcelDna.ComInterop;
+using ExcelDna.Integration.CustomUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewComServer
+{
+    [ComVisible(true)]
+    public class Ribbon : ExcelRibbon
+    {
+        public override string GetCustomUI(string RibbonID)
+        {
+            return 
+                @"<customUI xmlns='http://schemas.microsoft.com/office/2009/07/customui'>
+                  <ribbon>
+                    <tabs>
+                      <tab idMso='TabAddIns'>
+                        <group id='GroupExcelDnaCom' label='Excel-DNA COM'>
+                          <button id='ButtonRegister' imageMso='HappyFace' size='large' label='Register' onAction='Register_Click'/>
+                          <button id='ButtonUnregister' imageMso='Delete' size='large' label='Unregister' onAction='Unregister_Click'/>
+                        </group>
+                      </tab>
+                    </tabs>
+                  </ribbon>
+                </customUI>";
+        }
+
+        public void Register_Click(IRibbonControl control)
+        {
+            ComServer.DllRegisterServer();
+        }
+
+        public void Unregister_Click(IRibbonControl control)
+        {
+            ComServer.DllUnregisterServer();
+        }
+    }
+}
